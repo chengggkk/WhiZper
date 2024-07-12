b252fcf3d3d56f7a3a92981bda839cb38549e4f8
chengggkk <153604637+chengggkk@users.noreply.github.com>
2024-07-12 21:35:47 +0200

    init with polygonid flutter sdk example

---
+++ lib/src/presentation/ui/splash/splash_state.dart
@@ -0,0 +1,49 @@
+abstract class SplashState {
+  SplashState();
+
+  factory SplashState.init() => InitSplashState();
+
+  factory SplashState.waitingTimeEnded() => WaitingTimeEndedSplashState();
+
+  factory SplashState.downloadProgress({
+    required int downloaded,
+    required int contentLength,
+  }) =>
+      DownloadProgressSplashState(
+        downloaded: downloaded,
+        contentLength: contentLength,
+      );
+
+  factory SplashState.error({
+    required String errorMessage,
+  }) =>
+      ErrorSplashState(
+        errorMessage: errorMessage,
+      );
+}
+
+class InitSplashState extends SplashState {
+  InitSplashState();
+}
+
+class WaitingTimeEndedSplashState extends SplashState {
+  WaitingTimeEndedSplashState();
+}
+
+class DownloadProgressSplashState extends SplashState {
+  final int downloaded;
+  final int contentLength;
+
+  DownloadProgressSplashState({
+    required this.downloaded,
+    required this.contentLength,
+  });
+}
+
+class ErrorSplashState extends SplashState {
+  final String errorMessage;
+
+  ErrorSplashState({
+    required this.errorMessage,
+  });
+}
