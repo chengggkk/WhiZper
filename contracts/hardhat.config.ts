b3396effc404734fbce1a1cb9d2fed86a16e176c
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2024-07-12 23:30:10 +0200

    feat: add contract deployment

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,35 @@
+import dotenv from 'dotenv';
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-verify';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [
+        `${process.env.PRIVATE_KEY}`
+      ]
+    },
+    amoy: {
+      url: `https://polygon-amoy.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [
+        `${process.env.PRIVATE_KEY}`
+      ]
+    },
+    arbitrum: {
+      url: `https://arbitrum-sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [
+        `${process.env.PRIVATE_KEY}`
+      ]
+    }
+  }
 };
 
 export default config;
